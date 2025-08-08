13a24bcbbf9da07369b5b83f5b910f0dca52ccbe
FerVzq <natalyvazquez545@gmail.com>
2025-08-07 22:07:05 -0600

    Se agregan los métodos para imprimir y generar los arreglos

--- src/org/utl/ldsm304/actividad6/vista/VistaPrincipal.java
+++ src/org/utl/ldsm304/actividad6/vista/VistaPrincipal.java
@@ -4,15 +4,57 @@
  */
 package org.utl.ldsm304.actividad6.vista;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author natal
  */
 public class VistaPrincipal {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static void imprimirArrayResultante(ArrayList<String> arrayResultante){
+          for (int i = 0; i < arrayResultante.size(); i++) {
+            System.out.println(arrayResultante.get(i));
+        }
+    }
+    
+    public static double[][] generarArregloPrecios(){
+        double[][] precios = {
+            {10.0, 10.0, 10.0, 13.0},
+            {20.0, 25.0, 32.0, 41.0},
+            {50.0, 33.0, 10.0, 25.0},
+            {5.0, 21.0, 15.0, 10.0}
+        };
+        
+        return precios;
+    }
+    
+    public static String[][] generarArregloCadenas(){
+        String[][] palabras = {
+            {"oso", "universo", "camiseta"},
+            {"estupendo", "murcielago", "oracion"},
+            {"salado", "sabana", "murga"}
+         };
+        return palabras;
+    }
+    
+    public static void imprimirArregloPrecios(double[][] matriz) {
+    for (int i = 0; i < matriz.length; i++) {
+        for (int j = 0; j < matriz[i].length; j++) {
+            System.out.print(matriz[i][j] + "\t");
+        }
+        System.out.println(); 
+    }
+}
+    
+     public static void imprimirArregloCadenas(String[][] matriz) {
+    for (int i = 0; i < matriz.length; i++) {
+        for (int j = 0; j < matriz[i].length; j++) {
+            System.out.print(matriz[i][j] + "\t");
+        }
+        System.out.println();
+}
+}
     public static void main(String[] args) {
           System.out.println("Estoy probando que se pueda actualizar el repositorio con nuevos cambios");
           
