987d178a77af386d1459eb0786ecb4706d21ef2e
FerVzq <natalyvazquez545@gmail.com>
2025-08-07 20:41:13 -0600

    Se agrega el metodo a del arreglo bidimensional de cadenas

--- src/org/utl/ldsm304/actividad6/controlador/ControladorArregloCadenas.java
+++ src/org/utl/ldsm304/actividad6/controlador/ControladorArregloCadenas.java
@@ -4,10 +4,25 @@
  */
 package org.utl.ldsm304.actividad6.controlador;
 
+import java.util.ArrayList;
+
+
 /**
  *
  * @author natal
  */
 public class ControladorArregloCadenas {
-    
+    public ArrayList<String> devolverCadenasConO(String[][] arreglo){
+        ArrayList<String> resultado = new ArrayList<>();
+        for (int i = 0; i < arreglo.length ; i++) {
+            for (int j = 0; j < arreglo[i].length; j++) {
+                String palabra = arreglo[i][j].toLowerCase();
+                char letra = palabra.charAt(0);
+                if(letra == 'o'){
+                    resultado.add(palabra);
+                }
+            }
+        }
+        return resultado;
+    }
 }
